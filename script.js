aacd2f0eedc24254dba543899dfa1175fc07b0fa
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-09-22 23:51:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let ref = document.getElementById("browser-info")
+    document.write("You are using");
+	document.write(navigator.appName);
+    document.write(navigator.appVersion);
